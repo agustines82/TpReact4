0019af18376be842ad85028318c7a56285380347
Agustin Sampayo <agustines82@gmail.com>
2022-09-21 20:44:06 -0300

    guardo el valor del input en un arreglo y limpio el formulario

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -2,14 +2,21 @@ import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import ListaTarea from "./ListaTarea";
 const FormularioTarea = () => {
-    const [stateTareaInput, setStateTareaInput] = useState("");
-
+    const [tareaInputState, setTareaInputState] = useState("");
+    const [listaTareasState, setListaTareasState] = useState([]);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        //guardo el valor del input en el arreglo
+        setListaTareasState(...listaTareasState, tareaInputState);
+        //limpio el input
+        setTareaInputState("");
+    };
     return (
         <>
             <p className="container text-center lead">Ingresa tus tareas</p>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="tareas">
-                    <Form.Control type="text" placeholder="Tarea 1..." onChange={(e) => setStateTareaInput(e.target.value)} />
+                    <Form.Control type="text" placeholder="Tarea 1..." onChange={(e) => setTareaInputState(e.target.value)} />
                 </Form.Group>
             </Form>
             <ListaTarea />
